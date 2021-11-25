4536e69d9d6ad620bbe979a1c48c4368bb26d602
okazaki05 <okazaki.yuuya0412@gmail.com>
2021-11-25 09:34:34 +0900

    うんち

--- app/src/main/java/com/websarva/wings/android/alarm_register/Alarm_register.java
+++ app/src/main/java/com/websarva/wings/android/alarm_register/Alarm_register.java
@@ -2,7 +2,13 @@ package com.websarva.wings.android.alarm_register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Alarm_register extends AppCompatActivity {
 
@@ -11,4 +17,22 @@ public class Alarm_register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_register);
     }
+
+    public void nya(View v){
+
+        final Calendar calendar = Calendar.getInstance();
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        final TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Toast.makeText(Alarm_register.this, hourOfDay+"時"+minute+"分", Toast.LENGTH_LONG).show();
+
+                    }
+                }, hour, minute, true);
+        timePickerDialog.show();
+    }
+
 }
